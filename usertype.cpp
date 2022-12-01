7864a3a0a12717488f25a7efdef4cddcd8878c36
UNKNOWN <103091175+Faleupolu@users.noreply.github.com>
2022-12-01 18:30:15 +1300

    Create usertype.cpp

---
+++ usertype.cpp
@@ -0,0 +1,33 @@
+#include "usertype.h"
+
+userType::userType(QString user, QString pass, QString fName, QString lName, QString gender, QString age){
+
+    this->setUsername(user);
+    this->setPassword(pass);
+    this->firstName = fName;
+    this->lastName = lName;
+    this->gender = gender;
+    this-> age = age;
+
+    // New users by default have 0 vaccination and no booster shots. 1st & 2nd will dictate state - Unvacinnated / Partial / Vaccinated
+
+    this->firstVac = false;
+    this->secondVac = false;
+    this->boosterVac = false;
+
+    // No QR Code by default
+
+    imageQR = false;
+
+}
+
+QString userType::getVaccinationStatus()
+{
+
+    if(firstVac == true && secondVac == false){return QString("Partial");}
+    else if(firstVac == true && secondVac == true){return QString("Completed");}
+    else{return QString("Unvaccinated");}
+
+}
+
+
